da6abbd47f85c2144cf7428a65b702d980b90502
Dale <Danntabachoyy@gmail.com>
2024-02-05 12:33:52 +0800

    added redux persist

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -1,8 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from './users/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+const rootReducer = combineReducers({ user: userReducer });
+const persistedReducer = persistReducer(
+  { key: 'root', storage, version: 1 },
+  rootReducer
+);
 const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer,
 });
+export const persistor = persistStore(store);
 export default store;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { Provider } from 'react-redux';
-import store from './features/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './features/store.js';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate persistor={persistor} loading={null}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
