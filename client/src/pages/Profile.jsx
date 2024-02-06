343c3ba85db983b7691f9649ff0e8b89b8720ef0
Dale <Danntabachoyy@gmail.com>
2024-02-07 07:10:15 +0800

    @client update profile

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -7,8 +7,20 @@ function Profile() {
   const [editActive, setEditActive] = useState(false);
   const [displayName, setdisplayName] = useState(currentUser.displayName);
   const imageRef = useRef(null);
+
   const handleImageClick = () => {
     imageRef.current.click();
+  };
+  const handleClickEdit = () => {
+    setEditActive((val) => !val);
+    setdisplayName(currentUser.displayName);
+  };
+  const handleChangeDisplayName = (e) => {
+    const { value } = e.target;
+    setdisplayName(value);
+  };
+  const handleClickSave = () => {
+    console.log('clicked');
   };
   return (
     <div className="flex justify-center">
@@ -31,13 +43,11 @@ function Profile() {
               <div>
                 <input
                   type="text"
-                  className="text-2xl font-bold"
+                  className="text-2xl font-bold border rounded"
                   name="displayName"
                   value={displayName}
-                  // dito nako
-                  // onChange={}
+                  onChange={(e) => handleChangeDisplayName(e)}
                 />
-                <input type="text" />
               </div>
             ) : (
               <div>
@@ -47,9 +57,32 @@ function Profile() {
                 <h3 className="text-sm ">{currentUser.username}</h3>
               </div>
             )}
-            <div className="flex items-center mt-2">
-              <FaRegEdit className="hover:text-blue-500 text-xl cursor-pointer" />
-              <MdDeleteOutline className="hover:text-red-500 text-2xl cursor-pointer" />
+            <div className="flex items-center mt-2 space-x-2">
+              {editActive ? (
+                <>
+                  <button
+                    className="py-1 px-3 hover:bg-red-600 bg-red-500 text-white rounded"
+                    onClick={handleClickEdit}
+                  >
+                    cancel
+                  </button>
+                  <button
+                    className="py-1 px-3 hover:bg-blue-600 bg-blue-500 text-white rounded disabled:cursor-not-allowed disabled:opacity-80 disabled:hover:bg-blue-500"
+                    disabled={!displayName}
+                    onClick={handleClickSave}
+                  >
+                    save
+                  </button>
+                </>
+              ) : (
+                <>
+                  <FaRegEdit
+                    className="hover:text-blue-500 text-xl cursor-pointer"
+                    onClick={handleClickEdit}
+                  />
+                  <MdDeleteOutline className="hover:text-red-500 text-2xl cursor-pointer" />
+                </>
+              )}
             </div>
           </div>
         </div>
